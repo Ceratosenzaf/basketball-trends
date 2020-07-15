baed0d1e3ae92859356e620823b4965a1665e54c
Davide Cerato <mrc3drik@gmail.com>
2020-07-15 18:13:10 +0200

    visualizing players Salary

--- trends.py
+++ trends.py
@@ -1,5 +1,22 @@
 from data import *
 import numpy as np
+import matplotlib.pyplot as plt
 
-my_data = np.arange(0,20)
-my_matrix = my_data.reshape((5,4))
+
+def draw_player(scope, player, color = None, line = '-'):
+    plt.plot(scope, label = player, c = color, ls = line)
+
+
+if __name__ == "__main__":
+    # add player to chart
+    for player in Players:
+        draw_player(Salary[Pdict[player]], player)
+
+    # add legend to chart with its upper left corner to 1,1
+    plt.legend(loc = 'upper left', bbox_to_anchor =  (1,1))
+
+    # add seasons label
+    plt.xticks(list(range(0,10)), Seasons, rotation = 'vertical')
+
+    #visualize graph
+    plt.show()
